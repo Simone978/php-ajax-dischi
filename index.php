d406ecdbffa9c95c79e560fee1b7390f4865b6a1
Simone <simonelega.sl@gmail.com>
2020-02-25 15:35:21 +0100

    Creata e formattata barra header

--- index.php
+++ index.php
@@ -14,18 +14,27 @@ require __DIR__.'/database.php';
   <title>Document</title>
 </head>
 <body>
-  <div class="wrapper">
-
-    <?php foreach ($dischi as $key => $disco) { ?>
-      <div class="dischi_container">
-      <img src="<?php echo $disco['poster'] ?>" alt="">
-      <h2><?php echo $disco['title'] ?></h2>
-      <h4><?php echo $disco['author'] ?></h4>
-      <h5><?php echo $disco['year'] ?></h5>
+  <header>
+    <div class="wrapper_bar">
+      <div class="bar">
+        <img src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-marilyn-scott-0.png" alt="">
       </div>
-    <?php } ?>
+    </div>
+  </header>
+  
+  <main>
+    <div class="wrapper">
+      <?php foreach ($dischi as $key => $disco) { ?>
+        <div class="dischi_container">
+        <img src="<?php echo $disco['poster'] ?>" alt="">
+        <h2><?php echo $disco['title'] ?></h2>
+        <h4><?php echo $disco['author'] ?></h4>
+        <h5><?php echo $disco['year'] ?></h5>
+        </div>
+      <?php } ?>
+    </div>
+  </main>
 
-  </div>
 
 
 </body>
