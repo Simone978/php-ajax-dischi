1eceb638d2bb142867eb8321c682337e5ed49f87
Simone <simonelega.sl@gmail.com>
2020-02-25 15:16:47 +0100

    Effettuata chiamata al fake/db e formattati i risultati con sass

---
+++ database.php
@@ -0,0 +1,58 @@
+<?php
+
+$dischi = [
+  [
+    'title' => 'New Jersey',
+    'author' => 'Bon Jovi',
+    'year' => 1988,
+    'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+  ],
+  [
+    'title' => 'Cross Road',
+    'author' => 'Bon Jovi',
+    'year' => 1995,
+    'poster' => 'https://img.discogs.com/jW4A6C8Pw-Q98U_5nLAWb5b9KIo=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-5235591-1516699201-1953.jpeg.jpg'
+  ],
+  [
+    'title' => 'Live at Wembley',
+    'author' => 'Queen',
+    'year' => 1986,
+    'poster' => 'https://img.discogs.com/CcCegFMy3-1MiOBdqsf8MZurlIQ=/fit-in/600x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-887741-1403548494-3773.jpeg.jpg'
+  ],
+  [
+    'title' => 'Disintegration',
+    'author' => 'The Cure',
+    'year' => 1989,
+    'poster' => 'https://img.discogs.com/ZRH1-2eoqe6-w5YqlxQMaF8zfqo=/fit-in/550x550/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-264583-1384695510-4715.jpeg.jpg'
+  ],
+  [
+    'title' => 'Violator',
+    'author' => 'Depeche Mode',
+    'year' => 1990,
+    'poster' => 'https://img.discogs.com/DdVrEnmCSuQ7jOBsNBc1at14qqI=/fit-in/600x606/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-46905-1364082461-3242.jpeg.jpg'
+  ],
+  [
+    'title' => 'In The Flat Field',
+    'author' => 'Bauhaus',
+    'year' => 1980,
+    'poster' => 'https://img.discogs.com/_S7-jiD6XaJdgWs3NmUeaj34Z6E=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-379701-1314042510.jpeg.jpg'
+  ],
+  [
+    'title' => 'Closer',
+    'author' => 'Joy Division',
+    'year' => 1980,
+    'poster' => 'https://img.discogs.com/4VnaPrww7S6I9z0iZxoGsDvP-Vo=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-81692-1538992531-7551.jpeg.jpg'
+  ],
+  [
+    'title' => 'Closer',
+    'author' => 'Joy Division',
+    'year' => 1980,
+    'poster' => 'https://img.discogs.com/4VnaPrww7S6I9z0iZxoGsDvP-Vo=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-81692-1538992531-7551.jpeg.jpg'
+  ],
+  [
+    'title' => 'Closer',
+    'author' => 'Joy Division',
+    'year' => 1980,
+    'poster' => 'https://img.discogs.com/4VnaPrww7S6I9z0iZxoGsDvP-Vo=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-81692-1538992531-7551.jpeg.jpg'
+  ],
+];
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+
+<?php
+require __DIR__.'/database.php';
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +14,19 @@
   <title>Document</title>
 </head>
 <body>
+  <div class="wrapper">
+
+    <?php foreach ($dischi as $key => $disco) { ?>
+      <div class="dischi_container">
+      <img src="<?php echo $disco['poster'] ?>" alt="">
+      <h2><?php echo $disco['title'] ?></h2>
+      <h4><?php echo $disco['author'] ?></h4>
+      <h5><?php echo $disco['year'] ?></h5>
+      </div>
+    <?php } ?>
+
+  </div>
+
 
 </body>
 </html>
